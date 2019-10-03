75b2e775fdd3f93e88b7c4013fe1f7643cbf59e5
Dries De Peuter <dries@nousefreak.be>
2019-10-04 00:17:56 +0200

    Start using letitgo to do releases

---
+++ scripts/release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e 
+
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+cd "$DIR"
+
+curl -sL http://bit.ly/gh-get | BIN_DIR=/tmp/bin PROJECT=NoUseFreak/letitgo bash
+
+/tmp/bin/letitgo $(git describe --tags --abbrev=0)
